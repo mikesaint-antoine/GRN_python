55d14f2363b8a29dac3f91f2fb18030fed62a643
Mike Saint-Antoine <mikesaint-antoine@Mikes-MacBook-Pro.local>
2022-12-18 23:08:23 -0500

    start simulation file

---
+++ testing_simulations.py
@@ -0,0 +1,25 @@
+import numpy as np
+import simulation
+import matplotlib.pyplot as plt
+import sys
+
+
+
+
+
+[G1,G2,t] = simulation.sim_toggle_switch(plot_fig=1)
+
+
+print(len(G1))
+print(len(G2))
+print(len(t))
+
+
+# f, (ax1, ax2) = plt.subplots(2, sharex=True, sharey=False)
+# line1, = ax1.plot(t , G1, color="b",label="G1")
+# line2, = ax2.plot(t , G2, color="r",label="G2")
+# ax1.set_ylabel('G1')
+# ax2.set_ylabel('G2')
+
+# ax2.set_xlabel('Time')
+# plt.show()
